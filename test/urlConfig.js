811112243268929edd822c426afc1d46413ddb54
Peter Sjögårde <peter.sjogarde@ki.se>
2021-02-14 20:51:28 +0100

    urlConfig.js added

---
+++ test/urlConfig.js
@@ -0,0 +1,4 @@
+var urlPackages = "https://petersjogarde.github.io/packages/v1/";
+if ((window.location.hostname=="localhost") || (window.location.hostname=="127.0.0.1")) {
+ urlPackages = "http://"+window.location.hostname+":"+window.location.port+"/network_packages/";
+}
