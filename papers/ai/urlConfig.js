8d01af5544b052202cf0833fc7d7251212e732fb
Peter Sjögårde <peter.sjogarde@ki.se>
2022-01-14 13:24:49 +0100

    changed path

--- papers/ai/urlConfig.js
+++ papers/ai/urlConfig.js
@@ -1,4 +1,1 @@
-var urlPackages = "https://bibliometrics.ki.se/analysis_files/packages/v1/";
-if ((window.location.hostname=="localhost") || (window.location.hostname=="127.0.0.1")) {
- urlPackages = "http://"+window.location.hostname+":"+window.location.port+"/network_packages/";
-}
+var urlPackages = "https://petersjogarde.github.io/packages/v1/";
